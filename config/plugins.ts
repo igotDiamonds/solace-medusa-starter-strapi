862246ee32c21fc4cb34d8cc2ddba578696b9dcc
Andrey <brin3oll@gmail.com>
2025-04-03 16:27:21 +0300

    fix: build breakign issue

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,34 +1,35 @@
-const isS3 = env("DO_SPACE_SECRET_KEY") && env("DO_SPACE_ENDPOINT")
-
-export default ({ env }) => ({
-  upload: {
-    config: isS3
-      ? {
-          provider: "aws-s3",
-          providerOptions: {
-            rootPath: env("DO_SPACE_PATH"),
-            credentials: {
-              accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-              secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+export default ({ env }) => {
+  const isS3 = env("DO_SPACE_SECRET_KEY") && env("DO_SPACE_ENDPOINT")
+  return {
+    upload: {
+      config: isS3
+        ? {
+            provider: "aws-s3",
+            providerOptions: {
+              rootPath: env("DO_SPACE_PATH"),
+              credentials: {
+                accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+                secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+              },
+              region: env("DO_SPACE_REGION"),
+              endpoint: env("DO_SPACE_ENDPOINT"),
+              params: {
+                Bucket: env("DO_SPACE_BUCKET"),
+              },
             },
-            region: env("DO_SPACE_REGION"),
-            endpoint: env("DO_SPACE_ENDPOINT"),
-            params: {
-              Bucket: env("DO_SPACE_BUCKET"),
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          }
+        : {
+            providerOptions: {
+              localServer: {
+                maxage: 300000,
+              },
             },
           },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
-        }
-      : {
-          providerOptions: {
-            localServer: {
-              maxage: 300000,
-            },
-          },
-        },
-  },
-})
+    },
+  }
+}
