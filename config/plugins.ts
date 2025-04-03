45d09840c22e492ac78693169ad1417ba48da9fe
Andrey <brin3oll@gmail.com>
2025-04-03 16:21:41 +0300

    feat: add ability to store locally

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,34 @@
+const isS3 = env("DO_SPACE_SECRET_KEY") && env("DO_SPACE_ENDPOINT")
+
 export default ({ env }) => ({
   upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        rootPath: env("DO_SPACE_PATH"),
-        credentials: {
-          accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-          secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+    config: isS3
+      ? {
+          provider: "aws-s3",
+          providerOptions: {
+            rootPath: env("DO_SPACE_PATH"),
+            credentials: {
+              accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+              secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+            },
+            region: env("DO_SPACE_REGION"),
+            endpoint: env("DO_SPACE_ENDPOINT"),
+            params: {
+              Bucket: env("DO_SPACE_BUCKET"),
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        }
+      : {
+          providerOptions: {
+            localServer: {
+              maxage: 300000,
+            },
+          },
         },
-        region: env("DO_SPACE_REGION"),
-        endpoint: env("DO_SPACE_ENDPOINT"),
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
   },
-});
+})
